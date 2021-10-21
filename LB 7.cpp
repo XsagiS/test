d9cd8075facaa97ded838480b1a7897e60464429
XsagiS <jorik23051983@gmail.com>
2021-10-21 23:12:13 +0300

    change code

--- LB 7.cpp
+++ LB 7.cpp
@@ -1,31 +1,29 @@
-﻿#include <iostream>
-#include <iomanip>
-#include "windows.h"
-#include "math.h"
-
+﻿#include "math.h"
+#include <iostream>
+#include "iomanip"
 using namespace std;
-int mult(int b, int a) {
-	int y;
-	y = sqrt(a * b);
-	return y;
-	
-}
-
-
 
 int main()
 {
-	
-	int a, b;
-	cout << "Enter a b:";
-	cin >> a >> b;
-	cout << mult(a, b);
-	
-	
+    double y, x;
+    cout << "Enter x: (rad): " << endl;
+    cin >> x;
+    y = 0;
+    for (int i = 1; i < 11; i++) {
+        y += i;
+    }
+    y += pow(fabs(x), 3) + 2 * pow(sin(x), 2);
+    cout << "Y=" << setprecision(3) << y << endl;
+    //////
+    float y, z;
+    cout << "Enter y:";
+    cin >> y;
+    z = 1;
+    for (int i = 1; i <= 11; i++) {
+        z += cos(i);
+    }
+    z = z / y + 2;
+    cout << "Z=" << setprecision(2) << z << endl;
 
-		
-	
-	
+
 }
-
-
