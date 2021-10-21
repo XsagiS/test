b2e7857fffd0d8c54fe114cbf4108fb15ccdc7e4
XsagiS <jorik23051983@gmail.com>
2021-10-21 22:54:19 +0300

    first commit

---
+++ LB 7.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "windows.h"
+#include "math.h"
+
+using namespace std;
+int mult(int b, int a) {
+	int y;
+	y = sqrt(a * b);
+	return y;
+	
+}
+
+
+
+int main()
+{
+	
+	int a, b;
+	cout << "Enter a b:";
+	cin >> a >> b;
+	cout << mult(a, b);
+	
+	
+
+		
+	
+	
+}
+
+
